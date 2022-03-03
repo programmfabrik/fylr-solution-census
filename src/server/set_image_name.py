5f4230c034ad0233bda33b61fd721e90a75fe983
Philipp Hempel <philipp.hempel@programmfabrik.de>
2022-03-03 16:14:32 +0100

    WIP: started working on handling of sequential numbers using specific objects; see #59959

--- src/server/set_image_name.py
+++ src/server/set_image_name.py
@@ -2,51 +2,97 @@
 
 
 import sys
-from urllib import response
+
 import util
 import json
 
 OBJECTTYPE = 'cs_image'
 NAME_FIELD = 'name'
 
+PLUGIN_REF = 'census.core.set_image_name'
+
+
 NAME_PREFIX = 'HU'
 
-if __name__ == '__main__':
 
-    try:
-        orig_data = json.loads(sys.stdin.read())
+@util.handle_exceptions
+def main():
 
+    orig_data = json.loads(sys.stdin.read())
+
+    # get the server url
+    api_url = util.get_json_value(orig_data, 'info.api_url')
+    if api_url is None:
+        util.return_error_response('info.api_url missing!')
+    api_url += '/api/v1'
+
+    # get a session token
+    access_token = util.get_json_value(
+        orig_data, 'info.api_user_access_token')
+    # # xxx
+    # access_token= 'Xkq_tiukcdy_4zJzo4EipNKatPGPpe17estLEMdLJQk.HCpv3YtUWdMRfdlXn4a-Oi7_I2ny-akSw9uenMaLHKY'
+    if access_token is None:
+        util.return_error_response('info.api_user_access_token missing!')
+
+    # iterate over objects and check if the name must be set
+    objects = util.get_json_value(orig_data, 'objects')
+    if not isinstance(objects, list):
+        util.return_response(orig_data)
+
+    # collect indices of objects that need to be updated
+    # determine the new offset of the sequence
+    indices_to_update = set()
+    for i in range(len(objects)):
+        obj = objects[i]
+
+        if not isinstance(obj, dict):
+            continue
+
+        objecttype = util.get_json_value(obj, '_objecttype')
+        if objecttype != OBJECTTYPE:
+            # another objecttype was inserted, nothing to do here
+            continue
+
+        # skip if not INSERT
+        version = util.get_json_value(obj, OBJECTTYPE + '._version')
+        if version != 1:
+            # object was updated, nothing to do here
+            continue
+
+        name = util.get_json_value(
+            obj,
+            '%s.%s' % (OBJECTTYPE, NAME_FIELD))
+        if name not in [None, '']:
+            # name is already set, nothing to do here
+            continue
+
+        indices_to_update.add(i)
+
+    if len(indices_to_update) < 1:
+        # no updates in objects necessary, just return the original data
+        util.return_response(orig_data)
+
+    # if any names need to be update, get the next offset in the sequence
+    seq = util.FylrSequence(api_url, PLUGIN_REF, access_token)
+    offset = seq.get_next_number()
+
+    util.write_tmp_file('set_image_name.log', [
+        'offset: ' + str(offset)
+    ], new_file=True)
+
+    # update the new sequence to check if it has not been changed by another instance, else xxxrepeatxxx
+    update_objects = False
+    # todo
+
+    # todo if the new sequence was updated, update the actual objects
+
+    if update_objects:
         data = []
-
-        # todo: get actual number
-        num = 1
-
-        objects = util.get_json_value(orig_data, 'objects')
-        if not isinstance(objects, list):
-            raise util.SkipDataException(
-                orig_data,
-                'expect objects in array'
-            )
-
         for i in range(len(objects)):
             obj = objects[i]
 
-            if not isinstance(obj, dict):
-                raise util.SkipDataException(
-                    orig_data,
-                    'object %d is not an object' % i
-                )
-
-            objecttype = util.get_json_value(obj, '_objecttype')
-            if objecttype != OBJECTTYPE:
-                # another objecttype was inserted, nothing to do here
-                data.append(obj)
-                continue
-
-            # skip if not INSERT
-            version = util.get_json_value(obj, OBJECTTYPE + '._version')
-            if version != 1:
-                # object was updated, nothing to do here
+            if i not in indices_to_update:
+                # object is not affected, just copy it into the response
                 data.append(obj)
                 continue
 
@@ -59,8 +105,8 @@ if __name__ == '__main__':
                 continue
 
             # replace the empty name with a prefix and a running number
-            name = '%s%06d' % (NAME_PREFIX, num)
-            num += 1
+            name = '%s%06d' % (NAME_PREFIX, offset)
+            offset += 1
 
             obj[OBJECTTYPE][NAME_FIELD] = name
 
@@ -71,13 +117,6 @@ if __name__ == '__main__':
 
         util.return_response(response)
 
-    except util.SkipDataException as e:
-        # will not change the data of the object(s)
-        util.return_response(e.data)
 
-    except util.VerboseException as e:
-        util.return_error_response(e.getMessage())
-
-    except Exception as e:
-        trace = util.print_traceback(e)
-        util.return_error_response('\n'.join(trace))
+if __name__ == '__main__':
+    main()
--- src/server/util.py
+++ src/server/util.py
@@ -1,45 +1,44 @@
 # encoding: utf-8
 
+from datetime import datetime
 import json
 import sys
 import traceback
+import requests
 
 
-def print_traceback(e):
-    exc_info = sys.exc_info()
-    stack = traceback.extract_stack()
-    tb = traceback.extract_tb(exc_info[2])
-    full_tb = stack[:-1] + tb
-    exc_line = traceback.format_exception_only(*exc_info[:2])
-    return [
-        repr(e),
-        traceback.format_list(full_tb) + exc_line
-    ]
+# helper functions
+
+def write_tmp_file(name, lines, new_file=False, dir='/tmp/'):
+    if not isinstance(lines, list):
+        lines = [lines]
+
+    lines = [str(datetime.now()), ''] + lines
+
+    if not dir.endswith('/'):
+        dir += '/'
+    with open(dir + name, 'w' if new_file else 'a') as tmp:
+        tmp.writelines(map(lambda l: l + '\n', lines))
 
 
-class SkipDataException(Exception):
-    def __init__(self, data, msg):
-        Exception()
-        self.msg = msg
-        self.data = data
+def handle_exceptions(func):
+    def func_wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            exc_info = sys.exc_info()
+            stack = traceback.extract_stack()
+            tb = traceback.extract_tb(exc_info[2])
+            full_tb = stack[:-1] + tb
+            exc_line = traceback.format_exception_only(*exc_info[:2])
 
-    def getMessage(self):
-        return self.msg
+            trace = [str(repr(e))] + traceback.format_list(full_tb) + exc_line
 
-    def __str__(self):
-        return self.getMessage()
-
-
-class VerboseException(Exception):
-    def __init__(self, msg):
-        Exception()
-        self.msg = msg
-
-    def getMessage(self):
-        return self.msg
-
-    def __str__(self):
-        return self.getMessage()
+            write_tmp_file(
+                'set_image_name_exception.log',
+                trace,
+                new_file=True)
+    return func_wrapper
 
 
 def get_json_value(js, path, expected=False):
@@ -48,7 +47,7 @@ def get_json_value(js, path, expected=False):
     for path_part in path_parts:
         if not isinstance(current, dict) or path_part not in current:
             if expected:
-                raise VerboseException('expected: {0}'.format(path_part))
+                raise Exception('expected: ' + path_part)
             else:
                 return None
         current = current[path_part]
@@ -56,7 +55,10 @@ def get_json_value(js, path, expected=False):
 
 
 def dumpjs(js, indent=4):
-    return json.dumps(js, indent=4)
+    return json.dumps(js, indent=indent)
+
+
+# plugin response functions
 
 
 def stdout(line):
@@ -77,3 +79,120 @@ def return_response(response):
 def return_error_response(error):
     stderr(error)
     exit(1)
+
+# fylr api functions
+
+
+def fylr_api_headers(access_token):
+    return {
+        'authorization': 'Bearer: ' + access_token,
+    }
+
+
+@handle_exceptions
+def get_from_api(api_url, path, access_token):
+    resp = requests.get(
+        api_url + '/' + path,
+        headers=fylr_api_headers(access_token))
+
+    return resp.text, resp.status_code, resp.url
+
+
+@handle_exceptions
+def post_to_api(api_url, path, access_token, payload=None):
+    resp = requests.post(
+        api_url + '/' + path,
+        headers=fylr_api_headers(access_token),
+        data=payload)
+
+    return resp.text, resp.status_code, resp.url
+
+
+# class to handle a sequence using specific objects
+
+
+class FylrSequence(object):
+
+    OT_SEQ = 'sequence'
+    OT_SEQ_REF = 'reference'
+    OT_SEQ_OFFSET = 'offset'
+
+    current_number = 1
+    obj_id = None
+    version = 1
+
+    def __init__(self, api_url, ref, access_token) -> None:
+        self.api_url = api_url
+        while self.api_url.endswith('/'):
+            self.api_url = self.api_url[:-1]
+        self.ref = ref
+        self.access_token = access_token
+
+    @handle_exceptions
+    def get_from_api(self, path):
+        return get_from_api(self.api_url, path, self.access_token)
+
+    @handle_exceptions
+    def post_to_api(self, path, payload=None):
+        return post_to_api(self.api_url, path, self.access_token, payload)
+
+    @handle_exceptions
+    def get_next_number(self):
+        path = 'db/' + self.OT_SEQ + '/_all_fields/list'
+        api_resp, statuscode, used_url = self.get_from_api(path)
+
+        if statuscode != 200:
+            raise Exception('invalid response: ' +
+                            str(statuscode) + ' - ' + api_resp)
+
+        if len(api_resp) < 1:
+            raise Exception('invalid response: expected non-empty body')
+
+        objects = json.loads(api_resp)
+        if not isinstance(objects, list):
+            raise Exception('invalid response: expected array - ' + api_resp)
+
+        for obj in objects:
+            if get_json_value(obj, self.OT_SEQ + '.' + self.OT_SEQ_REF) != self.ref:
+                continue
+
+            n = get_json_value(
+                obj, self.OT_SEQ + '.' + self.OT_SEQ_OFFSET)
+            if n is None:
+                n = 0
+            if n < 0:
+                n = 0
+
+            # update offset and object id
+            self.current_number = n
+
+            self.obj_id = get_json_value(obj, self.OT_SEQ + '._id')
+            self.version = get_json_value(obj, self.OT_SEQ + '._version')
+
+            break
+
+        return self.current_number + 1
+
+    @handle_exceptions
+    def update(self, new_number):
+        if new_number <= self.current_number:
+            return False
+
+        new_obj = {
+            '_objecttype': self.OT_SEQ,
+            '_mask': '_all_fields',
+            self.OT_SEQ: {
+                '_id': self.obj_id,
+                '_version': self.version + 1,
+                self.OT_SEQ_OFFSET: new_number,
+                self.OT_SEQ_REF: self.ref
+            }
+        }
+
+        resp, statuscode = post_to_api(
+            self.api_url, 'db/' + self.OT_SEQ,
+            self.access_token,
+            dumpjs([new_obj])
+        )
+
+        return statuscode == 200
